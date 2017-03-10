91afc63e1a3dfb6c9aab2f444857b28548a34f1a
Kajisav <dan@dephekt.net>
2017-03-10 16:32:20 -0600

    Handle parsing CLI arguments properly

--- mygpg.py
+++ mygpg.py
@@ -4,42 +4,27 @@ del absolute_import, print_function, unicode_literals
 
 #import gpg
 import sys
+from optparse import OptionParser
 #from lxml import etree
 
-# This is the example from the GPGME manual.
+def main():
+    parser = OptionParser()
+    parser.add_option("-c", "--config", dest="config", help="read config from FILE", metavar="FILE")
+    parser.add_option("-a", "--algo", dest="algo", help="Use the key algorithm ALGO when generating", metavar="ALGO")
+    (options, args) = parser.parse_args()
+    if options.config:
+        print(options.config)
+    elif options.algo:
+        print(options.algo)
+        
 def genkey(self, **kwargs):
     """Generate a GPG public and private keypair using the given arguments or config file."""
-    for name, value in kwargs.items():
-        if name == "config":
-            print("Config File: " + value)
-        elif name == "algo-main":
-            print("Key Algorithm: " + value)
-        elif name == "keysize-main":
-            print("Key Size (Primary): " + value)
-        elif name == "algo-sub":
-            print("Subkey Algorithm: " + value)
-        elif name == "keysize-sub":
-            print("Key Size (Subkey): " + value)
-        elif name == "name":
-            print("Common Name: " + value)
-        elif name == "userid":
-            print("User ID: " + value)
-        elif name == "comment":
-            print("Comment: " + value)
-        elif name == "duration":
-            print("Key Duration: " + value)
-        else:
-            print(kwargs.items)
-    
-    print(self)
-
-if __name__ == "__main__":
-    genkey(sys.argv)
-    
-    #tree = etree.parse(config)
-    
     #with gpg.Context() as c:
         #c.set_progress_cb(gpg.callbacks.progress_stdout)
         #c.op_genkey(parms, None, None)
         #print("Generated key with fingerprint {0}.".format(
-    #c.op_genkey_result().fpr))
+    #c.op_genkey_result().fpr))    
+
+if __name__ == "__main__":
+    main()
+    
